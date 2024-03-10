11bc50b89d1300506c30ef16b55141b7860923c5
Karim Tayie <karimsherif2597@gmail.com>
2024-03-10 21:07:32 +0200

    feat: Updated config

--- src/config.js
+++ src/config.js
@@ -1,26 +1,30 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: 'karimsherif2597@gmail.com',
 
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/KarimTayie',
     },
-    {
-      name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
-    },
-    {
-      name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
-    },
+    // {
+    //   name: 'Instagram',
+    //   url: 'https://www.instagram.com/bchiang7',
+    // },
+    // {
+    //   name: 'Twitter',
+    //   url: 'https://twitter.com/bchiang7',
+    // },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/karimtayie',
     },
+    // {
+    //   name: 'Codepen',
+    //   url: 'https://codepen.io/bchiang7',
+    // },
     {
-      name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
+      name: 'HackerRank',
+      url: 'https://www.hackerrank.com/profile/karimsherif2597',
     },
   ],
 
