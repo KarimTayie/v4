2830046b1e253f558848fc8be7eda3b17cb83e6f
Karim Tayie <karimsherif2597@gmail.com>
2024-03-14 23:20:13 +0200

    feat: Removed trailing slash from site url

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Karim Tayie',
     description:
       'Karim Tayie is a self-taught software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'http://karimtayie.com/', // No trailing slash allowed!
+    siteUrl: 'https://karimtayie.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@KarimTayie',
   },
